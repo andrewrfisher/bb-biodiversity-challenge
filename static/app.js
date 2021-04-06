e85aba116afe06a1e456a242e9e62545b92ca044
andrewrfisher <andrewf704@gmail.com>
2021-04-06 15:44:17 -0400

    more changes day 2

--- static/app.js
+++ static/app.js
@@ -48,9 +48,28 @@ var drawChart = function(x_data, y_data, hoverText, metadata) {
     //making an array of trace so it can be plotted
     var data2 = [trace2];
   
-    //plot with plotly
+    //plot with plotly...this time as "bubble"
     Plotly.newPlot('bubble', data2);
   
   
   };
 
+  //populating the dropdown menu so it will return the values of what user chooses
+  var populateDropdown = function(names) {
+
+    //selDataset is the id we are looking for
+    var selectTag = d3.select("#selDataset");
+    //various options in dropdown
+    var options = selectTag.selectAll('option').data(names);
+  
+    //actually populates the dropdown with all the values in the dataset
+    options.enter()
+        .append('option')
+        .attr('value', function(d) {
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        });
+  
+  };
